b6d9d2722b0b2a57bde9afe7509bd67c1ece8d2b
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-07-22 16:11:25 -0500

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -3,7 +3,7 @@ def call(Map params = [:]) {
     def installation = params.Installation ?: params.installation ?: 'OWASP'
     def reportPattern = params.ReportPattern ?: params.reportPattern ?: '**/dependency-check-report.xml'
     def additionalArguments = params.AdditionalArguments ?: params.additionalArguments ?: "--scan ${scanPath} --format XML --format HTML --out reports/dependency-check"
-
+ 
     dependencyCheck additionalArguments: additionalArguments, odcInstallation: installation
     dependencyCheckPublisher pattern: reportPattern
 
