9a8c414f2068412a46c8c1c67b009cc23a58dd7e
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-07-22 16:11:11 -0500

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,7 +1,7 @@
 def call(Map params = [:]) {
     def timeoutMinutes = params.TimeoutMinutes ?: params.timeoutMinutes ?: 2
     def abortPipeline = params.AbortPipeline == null ? true : params.AbortPipeline
- 
+
     timeout(time: timeoutMinutes as Integer, unit: 'MINUTES') {
         waitForQualityGate abortPipeline: abortPipeline
     }
